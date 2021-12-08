f0d004d70264459dd9ca044bf8d915d514aab6f4
Derek <yaoxuedr@gmail.com>
2021-12-08 11:06:23 +0800

    reset

--- eval.sh
+++ eval.sh
@@ -10,4 +10,3 @@ CUDA_LAUNCH_BLOCKING=1 python eval_textBPN.py --exp_name Totaltext --checkepoch 
 #################### MSRA-TD500 ######################################
 # test_size=(640,1024)--cfglib/option
 #CUDA_LAUNCH_BLOCKING=1 python eval_textBPN.py --exp_name TD500 --checkepoch 680 --dis_threshold 0.3 --cls_threshold 0.925 --test_size 640 1024 --gpu 1
-#test for git
