c727c135fa5ad204780e545618e51d47943fc59c
Shualite <Shualite@outlook.com>
2021-12-10 16:38:43 +0800

    update

--- eval_textBPN.py
+++ eval_textBPN.py
@@ -159,8 +159,8 @@ def main(vis_dir_path):
                               'TextBPN_{}_{}.pth'.format(model.backbone_name, cfg.checkepoch))
     #model_path = os.path.join(cfg.save_dir, cfg.exp_name, 'TD500_resnet50_200.pth')
 
-    #model.load_model(model_path)
-    model.load_state_dict(torch.load(model_path))
+    model.load_model(model_path)
+    #model.load_state_dict(torch.load(model_path))
     model = model.to(cfg.device) # copy to cuda
     model.eval()
     if cfg.cuda:
